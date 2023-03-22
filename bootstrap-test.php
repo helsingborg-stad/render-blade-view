5a235db60f2a8724f341d9395237b88e650a10d7
Thor Brink <thor.brink@helsingborg.se>
2023-03-22 07:18:45 +0000

    test: update bootstrap

--- bootstrap-test.php
+++ bootstrap-test.php
@@ -3,8 +3,9 @@
 /**
  * Set up component library autoloading.
  */
+
+require_once dirname(__FILE__) . '/vendor/helsingborg-stad/component-library/source/php/vendor/Psr4ClassLoader.php';
 define('COMPONENTLIBRARY_PATH', __DIR__ . '/vendor/helsingborg-stad/component-library/');
-require_once __DIR__ . '/vendor/helsingborg-stad/component-library/source/php/vendor/Psr4ClassLoader.php';
 $loader = new \ComponentLibrary\Vendor\Psr4ClassLoader();
 $loader->addPrefix('ComponentLibrary', COMPONENTLIBRARY_PATH);
 $loader->addPrefix('ComponentLibrary', COMPONENTLIBRARY_PATH . 'source/php/');
